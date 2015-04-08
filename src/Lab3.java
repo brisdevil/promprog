345a1181c46de1e9baa7f030a0d204e216e4f35d
brisdevil <brisdevil@gmail.com>
2015-04-08 14:59:16 +0300

    Доделал. Допилил наречия, теперь все готово, пошел лабу сдавать.

--- src/Lab3.java
+++ src/Lab3.java
@@ -9,11 +9,11 @@ public abstract class  Lab3 {
         String s = "Вадян пил вкусный чай и здорово работал теплой ночью, кидок.";
         System.out.println(getNumOfVerbs(s));
         System.out.println(getNumOfAdj(s));
+        System.out.println(getNumOfAdv(s));
     }
 
     public static int getNumOfVerbs(String string) {
         String regexVerb = "[а-яА-Я]+?((ть)|(л)|(ла)|(ло)|(ли)|(шь)|(ет)|(ют)|(ят)|(аю))";
-
         Pattern p = Pattern.compile(regexVerb + regexNotLetters);
         Matcher m = p.matcher(string);
 
@@ -38,6 +38,22 @@ public abstract class  Lab3 {
             System.out.println("We have found: " + m.group());
             numOfAdj++;
         }
+
         return numOfAdj;
+    }
+
+    public static int getNumOfAdv(String string) {
+        String regexAdv = "[а-яА-Я]{2,}?о";
+
+        Pattern p = Pattern.compile(regexAdv + regexNotLetters);
+        Matcher m = p.matcher(string);
+
+        int numOfAdv = 0;
+        while (m.find()) {
+            System.out.println("We have found: " + m.group());
+            numOfAdv++;
+        }
+
+        return numOfAdv;
     }
 }
